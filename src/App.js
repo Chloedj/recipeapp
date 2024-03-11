98732595afa2505a03198c7ba59aed05264a249e
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-03-11 16:43:51 +0000

    Added search function and recipe display

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import RecipeDisplay from './RecipeDisplay.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+  let appID = '5bb4e5f1';
+  let appKey = "97b712a9fdeff602778283dbe0ddc7cb";
+  const [food_recipes, setfood_recipes] = useState([]); 
+    const [search_recipe, setSearch_recipe] = useState(''); 
+    const [search_query, setSearch_Query] = useState('chicken'); 
 
-export default App;
+
+useEffect(() => { 
+        getRecipesFunction(); 
+    }, [search_query]); 
+  
+    const getRecipesFunction = async () => { 
+        const response = await fetch( 
+`https://api.edamam.com/search?q=${search_query}&app_id=${appID}&app_key=${appKey}` 
+        ); 
+        const data = await response.json(); 
+        setfood_recipes(data.hits); 
+    }; 
+  
+    const updateSearchFunction = (e) => { 
+        setSearch_recipe(e.target.value); 
+    }; 
+  
+    const getSearchFunction = (e) => { 
+        e.preventDefault(); 
+        setSearch_Query(search_recipe); 
+        setSearch_recipe(''); 
+    }; 
+  
+    return ( 
+        <div className="App"> 
+            <header className="Header"> 
+                  <h1>Recipe Finder</h1>
+                                           
+            </header> 
+            <div className="search-bar"> 
+                <form 
+                    onSubmit={getSearchFunction} 
+                    
+                > 
+                    <div className="search-input"> 
+                        <input 
+                            type="text"
+                            name="search"
+                            value={search_recipe} 
+                            onChange={updateSearchFunction} 
+                            placeholder="Search for recipes..."
+                            
+                        /> 
+                    </div> 
+                    <button 
+                        type="submit"
+                        
+                    > 
+                        Search Recipe 
+                    </button> 
+                </form> 
+            </div> 
+  
+            <div className="recipe-display"> 
+                         {food_recipes.map((recipe) => ( 
+                        <RecipeDisplay key={recipe.recipe.label} recipe={recipe.recipe} /> 
+                    ))} 
+                
+            </div> 
+        </div> 
+    ); 
+}; 
---
+++ src/RecipeDisplay.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function RecipeDisplay({ recipe }) {
+    console.log(recipe);
+    return ( 
+    <div className="recipe-card"> 
+      <div className="recipe-info"> 
+        <img 
+          className="recipe-image"
+          src={recipe.image} 
+          alt={recipe.label} 
+        /> 
+       <div className="recipe-type"> 
+  {recipe.dishType && recipe.dishType.length > 0 ? recipe.dishType[0] : 'N/A'} 
+</div>
+      </div> 
+      <div className="recipe-text"> 
+        <h1 className="recipe-name"> 
+          {recipe.label} 
+        </h1> 
+        <div className="ingredients">
+  <span><b>Ingredients:</b></span>
+  <ul>
+    {recipe.ingredientLines.map((ingredient, index) => (
+      <li key={index}>{ingredient}</li>
+    ))}
+  </ul>
+</div>
+        <div className="recipe-link"> 
+  <a 
+    href={recipe.url} 
+    target="_blank"
+    rel="noopener noreferrer"
+    className="recipe-button"
+  > 
+    View Recipe 
+  </a> 
+</div> 
+      </div> 
+    </div> 
+  ); 
+}
